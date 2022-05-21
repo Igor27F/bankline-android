e9673103df06ef0079d509e5262de5153970d760
Igor27F <Igor.2703@hotmail.com>
2022-05-21 11:00:04 -0300

    first commit

---
+++ app/src/main/java/me/dio/bankline/domain/Movimentacao.kt
@@ -0,0 +1,10 @@
+package me.dio.bankline.domain
+
+data class Movimentacao(
+    val id: Int,
+    val dataHora: String,
+    val descricao: String,
+    val valor: Double,
+    val tipo: TipoMovimentacao,
+    val idCorrentista: Int,
+)
